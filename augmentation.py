50f5c9798debd6ddde76797a080e80bb78cb537e
Ehtisham Ali <65615524+ehtishamaliawan@users.noreply.github.com>
2022-10-19 15:41:47 +0500

    Add files via upload

---
+++ augmentation.py
@@ -0,0 +1,6 @@
+import Augmentor
+p = Augmentor.Pipeline("C:\\Users\\Ehtisham Ali\\OneDrive\\Desktop\\Telenor Screenshots")
+p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+p.sample(500)
+p.process()
